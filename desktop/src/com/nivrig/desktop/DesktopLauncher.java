5c632c7ac8771dd7095767fd2ae2a54644a07b8c
John Girvin <hello@nivrig.com>
2016-02-03 13:03:15 +0000

    initial commit

---
+++ desktop/src/com/nivrig/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nivrig.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nivrig.BunnyMark;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BunnyMark(), config);
+	}
+}
